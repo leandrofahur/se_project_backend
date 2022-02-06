3ff15e6c21465b68a2bd58633c99c58ba67a8236
Leandro Machado <fahurleandro@gmail.com>
2022-02-05 19:42:34 -0800

    boiler plate spring boot project

---
+++ g2_scared_to_compile/src/main/java/com/example/demo/G2ScaredToCompileApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2ScaredToCompileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G2ScaredToCompileApplication.class, args);
+	}
+
+}
